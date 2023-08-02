3023b7cba30e473a55a5a74ea576270b1de82b8f
Ken Kurita <45981670+KenKurita@users.noreply.github.com>
2023-08-01 19:09:59 -0700

    completed axios function for contact me

--- src/client/components/LandingPage/RightButtonsFolder/Contact.jsx
+++ src/client/components/LandingPage/RightButtonsFolder/Contact.jsx
@@ -1,32 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import $ from 'jquery';
 import './contact.css';
 
 export default function Contact(props) {
-   const [name , setName] = useState('NAME');
-   const [email, setEmail] = useState('EMAIL');
-   const [subject, setSubject] = useState('SUBJECT');
-   const [message, setMessage] = useState('MESSAGE');
-   const [phone, setPhone] = useState('PHONE #');
+  const [name, setName] = useState('NAME');
+  const [email, setEmail] = useState('EMAIL');
+  const [subject, setSubject] = useState('SUBJECT');
+  const [message, setMessage] = useState('MESSAGE');
+  const [phone, setPhone] = useState('PHONE #');
 
   function sendMessage(e) {
     e.preventDefault()
-    const data = {sendName: name, sendEmail: email, sendPhone: phone, sendSubject: subject, sendMess: message};
+    const data = { service_id: 'service_ifsexvs', template_id: 'template_ucuxej1', user_id: 'rXhqeo90gRIuOc5K1', template_params: { sendName: name, sendEmail: email, sendPhone: phone, sendSubject: subject, sendMess: message } };
     let x = '';
     setName('NAME')
-      setEmail('EMAIL')
-      setSubject('SUBJECT')
-      setMessage('MESSAGE')
-      setPhone('PHONE #')
-    axios.post('/contactMe', {data})
-    .then((response) => {
-      alert('Message successfully sent!')
-    })
-    .catch((err) => {
-      if (err) {
-        console.log(err, 'error inside contactMe Client')
-      }
-    })
+    setEmail('EMAIL')
+    setSubject('SUBJECT')
+    setMessage('MESSAGE')
+    setPhone('PHONE #')
+    // axios.post('/contactMe', { data })
+    //   .then((response) => {
+    //     alert('Message successfully sent!')
+    //   })
+    //   .catch((err) => {
+    //     if (err) {
+    //       console.log(err, 'error inside contactMe Client')
+    //     }
+    //   })
+
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json'
+    }).done(function () {
+      alert('Your mail is sent!');
+    }).fail(function (error) {
+      alert('Oops... ' + JSON.stringify(error));
+    });
   }
 
 
@@ -34,50 +45,50 @@ export default function Contact(props) {
     <div>
       <h1>CONTACT&nbsp;<div className="
       orange">ME</div></h1>
-    <span>
-      <div>
-        <div>DONT BE SHY!</div><br/>
-
-        <div> Feel free to get in touch with me. I am always open <br/> to discussing new projects, creative ideas or
-         <br/>opportunities to be part of your visions.</div><br/>
-
+      <span>
         <div>
-          <div>ADDRESS POINT</div>
-          San Leandro, CA 94578
-        </div><br/>
+          <div>DONT BE SHY!</div><br />
 
-        <div>
-          <div>MESSAGE ME</div>
-          kenivankurita@gmail.com
-        </div><br/>
+          <div> Feel free to get in touch with me. I am always open <br /> to discussing new projects, creative ideas or
+            <br />opportunities to be part of your visions.</div><br />
 
-        <div>
-          <div>CALL ME</div>
-          (209) 404-3734
+          <div>
+            <div>ADDRESS POINT</div>
+            San Leandro, CA 94578
+          </div><br />
+
+          <div>
+            <div>MESSAGE ME</div>
+            kenivankurita@gmail.com
+          </div><br />
+
+          <div>
+            <div>CALL ME</div>
+            (209) 404-3734
+          </div>
+
         </div>
+        <form action="#" method="post" onSubmit={sendMessage}>
+          <label>
+            <input type="text" id="name" name="name" value={name} required onChange={e => setName(e.target.value)} />
+            <br />
 
-      </div>
-    <form action="#" method="post" onSubmit={sendMessage}>
-      <label>
-        <input type="text" id="name" name="name" value={name} required onChange={e=> setName(e.target.value)}/>
-        <br/>
+            <input type="email" id="email" name="email" value={email} required onChange={e => setEmail(e.target.value)} />
+            <br />
 
-        <input type="email" id="email" name="email" value={email} required onChange={e=> setEmail(e.target.value)}/>
-        <br/>
+            <input type="phone" id="phone" name="phone" value={phone} required onChange={e => setPhone(e.target.value)} />
+            <br />
 
-        <input type="phone" id="phone" name="phone" value={phone} required onChange={e=> setPhone(e.target.value)}/>
-        <br/>
+            <input type="text" id="subject" name="subject" value={subject} required onChange={e => setSubject(e.target.value)} />
+            <br />
 
-        <input type="text" id="subject" name="subject" value={subject} required onChange={e=> setSubject(e.target.value)}/>
-        <br/>
+            <textarea id="message" name="message" rows="4" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+            <br />
 
-        <textarea id="message" name="message" rows="4" value={message} onChange={e=> setMessage(e.target.value)}></textarea>
-        <br/>
-
-        <button type="submit">Send</button>
-      </label>
-    </form>
-    </span>
+            <button type="submit">Send</button>
+          </label>
+        </form>
+      </span>
     </div>
   )
 };
