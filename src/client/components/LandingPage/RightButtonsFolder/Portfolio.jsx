934132a1d26b48748aae286aa47c7df4d51ca764
Ken Kurita <45981670+KenKurita@users.noreply.github.com>
2023-08-05 14:13:14 -0700

    removed bad files

--- src/client/components/LandingPage/RightButtonsFolder/Portfolio.jsx
+++ src/client/components/LandingPage/RightButtonsFolder/Portfolio.jsx
@@ -7,17 +7,30 @@ export default function Portfolio(props) {
     {image: <img src={'https://i.ibb.co/ZcgLHDN/IMG-4362-Medium.png'}></img>, name: 'GamerCity', stack: 'Tech stack: React, Node.js, Express, Javascript, Mocha, PostgreSQL', github: 'https://github.com/KenKurita/boc-slate-fe'},
     {image: <img src={'https://i.ibb.co/Prv8L5q/IMG-4363-Medium.png'}></img>, name: 'Kivan', stack: 'Tech stack: React, Node.js, Express, Javascript, MySQL', github: 'https://github.com/KenKurita/Kivan'}])
 
-  function showList() {
-    const loopy = list.map((item, index) => {
-      return <li key={index}>{item.image}</li>
-    })
-    return (
-      <ul className="no-bullets">{loopy}</ul>
-    )
-  }
+    function showList() {
+      const loopy = list.map((item, index) => {
+        return (
+          <li key={index} className="project-item">
+            {item.image}
+            <div>
+              <h3 id='orange'>{item.name}</h3>
+              <div>{item.stack}</div>
+              <div>
+                <a href={item.github} target="_blank" rel="noopener noreferrer">
+                  GitHub Repo
+                </a>
+              </div>
+            </div>
+          </li>
+        );
+      });
+
+      return <ul className="no-bullets">{loopy}</ul>;
+    }
+
 
   return (
-    <div>
+    <div className="container2">
       <h1>MY &nbsp; <div id='orange'>PORTFOLIO</div></h1>
       <div>{showList()}</div>
     </div>
